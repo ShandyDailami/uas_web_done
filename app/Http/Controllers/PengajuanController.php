6fa4af9415c4599d27a5b0a6df96ce0942333729
USER <ikhtiarafakhrunisa@gmail.com>
2024-06-26 15:44:28 +0800

    Menambahkan controller

--- app/Http/Controllers/PengajuanController.php
+++ app/Http/Controllers/PengajuanController.php
@@ -13,11 +13,6 @@ class PengajuanController extends Controller
         return view('index', compact('pj'));
     }
 
-    /**
-    * Show the form for creating a new resource.
-    *
-    * @return \Illuminate\Http\Response
-    */
     public function create()
     {
         return view('create');
