35de3316ccba8f03a45bc8c409dd0bb07089b6f3
PiolaEvania <piolaevania@gmail.com>
2024-06-26 14:03:44 +0800

    Menambahkan migration dan model

--- app/Models/Mahasiswa.php
+++ app/Models/Mahasiswa.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class Mahasiswa extends Model
 {
     use HasFactory;
+
+    protected $primaryKey = 'id_mahasiswa';
+
+    protected $fillable = [
+        'nama_lengkap',
+        'no_telpon',
+        'email',
+        'masuk_sebagai',
+        'password',
+    ];
+
+    public function pengajuan()
+    {
+        return $this->hasOne(Pengajuan::class, 'mahasiswa_id', 'id_mahasiswa');
+    }
 }
--- app/Models/Pengajuan.php
+++ app/Models/Pengajuan.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class Pengajuan extends Model
 {
     use HasFactory;
+
+    protected $primaryKey = 'id_pengajuan';
+
+    protected $fillable = [
+        'jenis_pengajuan',
+        'tanggal_pengajuan',
+        'status',
+        'mahasiswa_id',
+    ];
+
+    public function mahasiswa()
+    {
+        return $this->hasOne(Mahasiswa::class, 'mahasiswa_id', 'id_mahasiswa');
+    }
 }
